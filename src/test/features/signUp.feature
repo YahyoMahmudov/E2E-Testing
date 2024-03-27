570b11120ccaaea78b65c1652b3e733762acc8c6
Ilhom <Ilhom.Islomov@inoviagroup.se>
2024-03-27 23:57:15 +0500

    fixed

--- src/test/features/signUp.feature
+++ src/test/features/signUp.feature
@@ -7,4 +7,12 @@ Feature: Sign up
             Given User is in landing page
              When User navigates to sign up page by clicking the sign-up button
              When User fill out the account registration form with valid information
+                  | Full Name        | John Doe   |
+                  | Company Name     | ABC Inc    |
+                  | CompanyDetails   | test       |
+                  | AccountDetails   | test       |
+                  | Country          | Germany    |
+                  | Default Currency | USD        |
+                  | Industry         | Shopping   |
+                  | Phone            | 1234567890 |
              Then The user should see a success message
--- src/test/steps/signUpSteps.ts
+++ src/test/steps/signUpSteps.ts
@@ -6,8 +6,22 @@ When('User navigates to sign up page by clicking the sign-up button', async func
   await basePage.signUpPage.navigateToRegisterPage();
 });
 
-When('User fill out the account registration form with valid information', async function () {
-  await basePage.signUpPage.registerUser();
+When('User fill out the account registration form with valid information', async function (dataTable) {
+  const data = dataTable.rowsHash();
+  const country = data['Country'];
+  const currency = data['Default Currency'];
+  const industry = data['Industry'];
+
+
+  await basePage.signUpPage.fillInputs(data['Full Name'], data['Company Name'], data['Phone'],data['CompanyDetails'], data['AccountDetails'])
+  await basePage.signUpPage.genereteEmail();
+  await basePage.signUpPage.agreePrivacyPolicy();
+  await basePage.signUpPage.selectCountry(country);
+  await basePage.signUpPage.selectCurrency(currency);
+  await basePage.signUpPage.selectIndustry(industry);
+  await basePage.signUpPage.clickRegisterButton();
+
+
 });
 
 Then('The user should see a success message', async function () {
