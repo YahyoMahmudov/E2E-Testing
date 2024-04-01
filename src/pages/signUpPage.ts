caf5a88f101fe154da392b6bb710afce5e5f499b
Ilhom <Ilhom.Islomov@inoviagroup.se>
2024-04-01 16:54:53 +0500

    fixed scenario and step definitions

--- src/pages/signUpPage.ts
+++ src/pages/signUpPage.ts
@@ -11,12 +11,9 @@ export default class SignUpPage {
   companyNameInput = this.page.locator("(//input[@class='el-input__inner'])[2]");
   companyEmailInput = this.page.locator("(//input[@class='el-input__inner'])[3]");
   confirmEmailInput = this.page.locator("//input[@data-element='email_confirm']");
-  country = this.page.locator("(//i[contains(@class,'el-select__caret el-input__icon')])[1]");
-  countryList = this.page.locator("(//ul[@class='el-scrollbar__view el-select-dropdown__list'])[3]");
-  currency = this.page.locator("(//input[@name='currency'])[2]");
-  currencyList = this.page.locator("(//ul[@class='el-scrollbar__view el-select-dropdown__list'])[3]");
-  industry = this.page.locator("(//span[text()='Industry']/following::input)[2]");
-  industrylist = this.page.locator("(//ul[@class='el-scrollbar__view el-select-dropdown__list'])[3]");
+  countryListDropdown = this.page.locator("(//i[contains(@class,'el-select__caret el-input__icon')])[1]");
+  currencyListDropdown = this.page.locator("(//i[contains(@class,'el-select__caret el-input__icon')])[2]");
+  industryListDropdown = this.page.locator("(//i[contains(@class,'el-select__caret el-input__icon')])[3]");
   phoneNumberInput = this.page.locator("(//span[text()='Phone']/following::input)[1]");
   termsAndConditions = this.page.locator("(//span[@class='el-checkbox__inner'])[1]");
   privacyPolicy = this.page.locator("(//span[@class='el-checkbox__inner'])[2]");
@@ -24,6 +21,7 @@ export default class SignUpPage {
   accountDetailsInput = this.page.locator("(//span[text()='Account details Label *']/following::input)[1]");
   companyDetailsInput = this.page.locator("(//span[text()='Company details Label edited *']/following::input)[1]");
   successMessage = this.page.locator('.open-access__title');
+
   public selectOption(option: string): Locator {
     return this.page.locator(`//ul/li/span[text()='${option}']`);
   }
--- src/test/steps/signUpSteps.ts
+++ src/test/steps/signUpSteps.ts
@@ -6,6 +6,7 @@ Then('User clicks {string} on {string}', async function (button, page) {
   const [buttonName, pageName] = basePage.wrapper.toCamelCase(button, page);
   await basePage.wrapper.waitAndClick(basePage[pageName][buttonName]);
 });
+
 Then('User fills {string} as {string} on {string}', async function (inputLocator, fillInput, page) {
   let gmail = fillInput;
   if (inputLocator.includes('Email Input')) {
@@ -17,15 +18,18 @@ Then('User fills {string} as {string} on {string}', async function (inputLocator
   const [placeholderName, pageName] = basePage.wrapper.toCamelCase(inputLocator, page);
   await basePage.wrapper.type(basePage[pageName][placeholderName], gmail);
 });
-When('User chooses {string} as {string} from {string} on {string}', async function (selectTab, option, dropdownList, page) {
+
+When('User chooses {string} from {string} on {string}', async function (option,selectTab, page) {
   const [openList, pageName] = basePage.wrapper.toCamelCase(selectTab, page);
   await basePage.wrapper.waitAndClick(basePage[pageName][openList]);
   await basePage.wrapper.waitAndClick(basePage[pageName].selectOption(option));
 });
+
 Then('User should agree to {string} on {string}', async function (button, page) {
   const [checkbox, pageName] = basePage.wrapper.toCamelCase(button, page);
   await basePage.wrapper.waitAndClick(basePage[pageName][checkbox]);
 });
+
 Then('User should see {string} on {string}', async function (successMessage, page) {
   const [message, pageName] = basePage.wrapper.toCamelCase(successMessage, page);
   const successElement = basePage[pageName][message];
