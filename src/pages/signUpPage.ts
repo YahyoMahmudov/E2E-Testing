416ed53f8203f1da0a2613af078f9ee2415b0d00
Ilhom <Ilhom.Islomov@inoviagroup.se>
2024-04-01 15:12:28 +0500

    fixed

--- src/pages/signUpPage.ts
+++ src/pages/signUpPage.ts
@@ -8,23 +8,22 @@ export default class SignUpPage {
     this.base = new PlaywrightWrapper(page);
   }
   fullNameInput = this.page.locator("(//input[@class='el-input__inner'])[1]");
-  companyName = this.page.locator("(//input[@class='el-input__inner'])[2]");
-  companyEmail = this.page.locator("(//input[@class='el-input__inner'])[3]");
-  confirmEmail = this.page.locator("//input[@data-element='email_confirm']");
-  choosesCountry = this.page.locator("(//i[contains(@class,'el-select__caret el-input__icon')])[1]");
+  companyNameInput = this.page.locator("(//input[@class='el-input__inner'])[2]");
+  companyEmailInput = this.page.locator("(//input[@class='el-input__inner'])[3]");
+  confirmEmailInput = this.page.locator("//input[@data-element='email_confirm']");
+  country = this.page.locator("(//i[contains(@class,'el-select__caret el-input__icon')])[1]");
   countryList = this.page.locator("(//ul[@class='el-scrollbar__view el-select-dropdown__list'])[3]");
-  choosesCurrency = this.page.locator("(//input[@name='currency'])[2]");
+  currency = this.page.locator("(//input[@name='currency'])[2]");
   currencyList = this.page.locator("(//ul[@class='el-scrollbar__view el-select-dropdown__list'])[3]");
-  choosesIndustry = this.page.locator("(//span[text()='Industry']/following::input)[2]");
+  industry = this.page.locator("(//span[text()='Industry']/following::input)[2]");
   industrylist = this.page.locator("(//ul[@class='el-scrollbar__view el-select-dropdown__list'])[3]");
-  phoneNumber = this.page.locator("(//span[text()='Phone']/following::input)[1]");
+  phoneNumberInput = this.page.locator("(//span[text()='Phone']/following::input)[1]");
   termsAndConditions = this.page.locator("(//span[@class='el-checkbox__inner'])[1]");
   privacyPolicy = this.page.locator("(//span[@class='el-checkbox__inner'])[2]");
   registerButton = this.page.locator("//button[contains(@class,'el-button el-button--primary')]");
-  accountDetailsLabel = this.page.locator("(//span[text()='Account details Label *']/following::input)[1]");
-  companyDetailsLabel = this.page.locator("(//span[text()='Company details Label edited *']/following::input)[1]");
+  accountDetailsInput = this.page.locator("(//span[text()='Account details Label *']/following::input)[1]");
+  companyDetailsInput = this.page.locator("(//span[text()='Company details Label edited *']/following::input)[1]");
   successMessage = this.page.locator('.open-access__title');
-
   public selectOption(option: string): Locator {
     return this.page.locator(`//ul/li/span[text()='${option}']`);
   }
--- src/test/steps/signUpSteps.ts
+++ src/test/steps/signUpSteps.ts
@@ -6,31 +6,26 @@ Then('User clicks {string} on {string}', async function (button, page) {
   const [buttonName, pageName] = basePage.wrapper.toCamelCase(button, page);
   await basePage.wrapper.waitAndClick(basePage[pageName][buttonName]);
 });
-Then('User fills {string} as {string} on {string}', async function (placeholder, input, page) {
-  const [placeholderName, pageName] = basePage.wrapper.toCamelCase(placeholder, page);
-  await basePage.wrapper.type(basePage[pageName][placeholderName], input);
-});
-
-Then('User enters {string} as {string} on {string}', async function (email, input, page) {
-  if (!randomNum) {
-    randomNum = Math.floor(Math.random() * 10000);
+Then('User fills {string} as {string} on {string}', async function (inputLocator, fillInput, page) {
+  let gmail = fillInput;
+  if (inputLocator.includes('Email Input')) {
+    if (!randomNum) {
+      randomNum = Math.floor(Math.random() * 10000);
+    }
+    gmail = fillInput + '+' + randomNum + '@gmail.com';
   }
-  const [pageName, placeholderName] = basePage.wrapper.toCamelCase(page, email);
-  const updatedInput = `${input}${randomNum}@gmail.com`;
-  await basePage.wrapper.type(basePage[pageName][placeholderName], updatedInput);
+  const [placeholderName, pageName] = basePage.wrapper.toCamelCase(inputLocator, page);
+  await basePage.wrapper.type(basePage[pageName][placeholderName], gmail);
 });
-
-When('User {string} as {string} from {string} on {string}', async function (selectTab, option, dropdownList, page) {
+When('User chooses {string} as {string} from {string} on {string}', async function (selectTab, option, dropdownList, page) {
   const [openList, pageName] = basePage.wrapper.toCamelCase(selectTab, page);
   await basePage.wrapper.waitAndClick(basePage[pageName][openList]);
   await basePage.wrapper.waitAndClick(basePage[pageName].selectOption(option));
 });
-
 Then('User should agree to {string} on {string}', async function (button, page) {
   const [checkbox, pageName] = basePage.wrapper.toCamelCase(button, page);
   await basePage.wrapper.waitAndClick(basePage[pageName][checkbox]);
 });
-
 Then('User should see {string} on {string}', async function (successMessage, page) {
   const [message, pageName] = basePage.wrapper.toCamelCase(successMessage, page);
   const successElement = basePage[pageName][message];
